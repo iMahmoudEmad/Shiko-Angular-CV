d7e61b1cc704bb661e5d079e26e7a7f3606f9674
mahmoud emad eldin <mahmouddev23@gmail.com>
2019-10-23 19:12:11 +0200

    Add titleColor variable

--- src/app/components/project-information/project-information.component.ts
+++ src/app/components/project-information/project-information.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProjectInformationComponent implements OnInit {
   @Input() information;
+  @Input() titleColor;
   constructor() { }
 
   ngOnInit() {
